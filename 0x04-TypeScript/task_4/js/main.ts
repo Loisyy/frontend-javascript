b3383a43635d2ca36212b11597a7be97a45b802c
Nkeiru Lois <nkeiruloisy@yahoo.com>
2025-11-09 17:03:26 +0100

    task 11

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -4,16 +4,11 @@
 /// <reference path="./subjects/Java.ts" />
 /// <reference path="./subjects/React.ts" />
 
-import { Teacher } from './subjects/Teacher';
-import { Cpp } from './subjects/Cpp';
-import { Java } from './subjects/Java';
-import { React } from './subjects/React';
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
 
-export const cpp = new Cpp();
-export const java = new Java();
-export const react = new React();
-
-export const cTeacher:Teacher = {
+export const cTeacher: Subjects.Teacher = {
     firstName: 'John',
     lastName: 'Doe',
     experienceTeachingC: 10,
